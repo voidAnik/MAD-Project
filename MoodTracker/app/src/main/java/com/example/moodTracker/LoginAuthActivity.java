f1321119668898823ea23e43c6de99aa5d54e638
Anik <voidAnik@users.noreply.github.com>
2020-10-08 22:20:29 +0600

    Update LoginAuthActivity.java

--- MoodTracker/app/src/main/java/com/example/moodTracker/LoginAuthActivity.java
+++ MoodTracker/app/src/main/java/com/example/moodTracker/LoginAuthActivity.java
@@ -9,9 +9,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,7 +16,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import java.util.Arrays;
 import java.util.List;
 
